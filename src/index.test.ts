a6d5c83416ba938a2545715b53e6ef87771ae4b7
Tiago Barbosa <tbarbos@hotmail.com>
2024-04-23 09:08:12 +0100

    test: :white_check_mark: add few more unit tests
    Signed-off-by: Tiago Barbosa <tbarbos@hotmail.com>

--- src/index.test.ts
+++ src/index.test.ts
@@ -34,7 +34,7 @@ describe("Merkle Tree", () => {
         expect(tree.depth()).toBe(2);
     });
 
-    it("validates that leaf node exists in tree", async () => {
+    it("Validates that leaf node exists in tree", async () => {
 
         // Arrange
         const emails: string[] = [
@@ -54,7 +54,75 @@ describe("Merkle Tree", () => {
         expect(proof.isSome()).toBeTruthy();
     });
 
-    it("validates that all 3 emails exists in tree", async () => {
+    it("Validates that leaf node does not exist in tree", async () => {
+
+        // Arrange
+        const emails: string[] = [
+            "email1@email.com",
+            "email2@email.com",
+            "email3@email.com"
+        ];
+
+        const newEmail: string = "missing@email.com";
+
+        const sha256 = buildHashFunction('sha-256');
+        const leafToValidate = await sha256(Buffer.from(newEmail));
+
+        // Act
+        const tree = await createMerkleTree(emails);
+        const proof = tree.getProof(leafToValidate);
+
+        // Assert
+        expect(proof.isNone()).toBeTruthy();
+    });
+
+    it("Validates that the proof is valid", async () => {
+
+        // Arrange
+        const emails: string[] = [
+            "email1@email.com",
+            "email2@email.com",
+            "email3@email.com"
+        ];
+
+        const sha256 = buildHashFunction('sha-256');
+        const leafToValidate = await sha256(Buffer.from(emails[0]));
+
+        // Act
+        const tree = await createMerkleTree(emails);
+        const rootHash = tree.getRootHash();
+        const proof = tree.getProof(leafToValidate);
+
+        const isValidProof = await tree.validateProof(proof.some(), leafToValidate, rootHash);
+
+        // Assert
+        expect(proof.isSome()).toBeTruthy();
+        expect(isValidProof).toBeTruthy();
+    });
+
+    it("Validates that rootHash changes when new leafs are added", async () => {
+
+        // Arrange
+        const emails: string[] = [
+            "email1@email.com",
+            "email2@email.com",
+            "email3@email.com"
+        ];
+
+        const newEmail: string = "email4@email.com";
+
+        // Act
+        const tree1 = await createMerkleTree(emails);
+        const rootHash1 = tree1.getRootHash();
+
+        const tree2 = await addLeavesToTree(tree1, [newEmail]);
+        const rootHash2 = tree2.getRootHash();
+
+        // Assert
+        expect(rootHash1 !== rootHash2).toBeTruthy();
+    });
+
+    it("Validates that the 3 emails provided exist in tree", async () => {
 
         // Arrange
         const emails: string[] = [
@@ -80,49 +148,5 @@ describe("Merkle Tree", () => {
         expect(proof1.isSome()).toBeTruthy();
         expect(proof2.isSome()).toBeTruthy();
         expect(proof3.isSome()).toBeTruthy();
-    });
-
-    it("validates that leaf node does not exist in tree", async () => {
-
-        // Arrange
-        const emails: string[] = [
-            "email1@email.com",
-            "email2@email.com",
-            "email3@email.com"
-        ];
-
-        const newEmail: string = "missing@email.com";
-
-        const sha256 = buildHashFunction('sha-256');
-        const leafToValidate = await sha256(Buffer.from(newEmail));
-
-        // Act
-        const tree = await createMerkleTree(emails);
-        const proof = tree.getProof(leafToValidate);
-
-        // Assert
-        expect(proof.isNone()).toBeTruthy();
-    });
-
-    it("validates that rootHash changes when new leafs are added", async () => {
-
-        // Arrange
-        const emails: string[] = [
-            "email1@email.com",
-            "email2@email.com",
-            "email3@email.com"
-        ];
-
-        const newEmail: string = "email4@email.com";
-
-        // Act
-        const tree1 = await createMerkleTree(emails);
-        const rootHash1 = tree1.getRootHash();
-
-        const tree2 = await addLeavesToTree(tree1, [newEmail]);
-        const rootHash2 = tree2.getRootHash();
-
-        // Assert
-        expect(rootHash1 !== rootHash2).toBeTruthy();
     });
 });
